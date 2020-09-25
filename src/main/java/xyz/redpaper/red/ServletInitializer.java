24d360c461064468c483374ef0dfbec6bf01c6f9
ÍõÐ˵Ï <wangxingdi@boco.com.cn>
2020-09-25 12:41:01 +0800

    first commit

---
+++ src/main/java/xyz/redpaper/red/ServletInitializer.java
@@ -0,0 +1,13 @@
+package xyz.redpaper.red;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RedApplication.class);
+    }
+
+}
---
+++ src/test/java/xyz/redpaper/red/RedApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.redpaper.red;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
